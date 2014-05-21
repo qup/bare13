d40a7ebb034241846b92237b44e78e2ea78f4d3f
Casper Beyer <cbeyer@localhost.localdomain>
2014-05-21 10:25:25 +0800

    Initial commit.

---
+++ Makefile
@@ -0,0 +1,16 @@
+build: node_modules
+	node build.js
+
+node_modules: package.json
+	npm install
+
+# Simple deploy push
+deploy: build
+	cd build && \
+	git init . && \
+	git add . && \
+	git commit -m "Update build."; \
+	git push "https://github.com/qup/bare13.git" master:gh-pages --force && \
+	rm -rf .git
+
+.PHONY: build
---
+++ build.js
@@ -0,0 +1,26 @@
+var metalsmith = require('metalsmith');
+
+var autoprefixer = require('metalsmith-autoprefixer');
+var fingerprint = require('metalsmith-fingerprint');
+var headings = require('metalsmith-headings');
+var templates = require('metalsmith-templates');
+var permalinks = require('metalsmith-permalinks');
+
+var site = metalsmith(__dirname);
+
+site.use(fingerprint({ pattern: 'stylesheets/screen.css' }));
+site.use(headings('h2'));
+site.use(autoprefixer());
+site.use(permalinks({
+   relative: false,
+}));
+
+site.use(templates({
+   engine: 'handlebars'
+}));
+
+site.build(function(err) {
+   if (err) {
+      throw err;
+   }
+});
